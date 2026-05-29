c17a81534293cf464141bca0293715a797d6860b
nandini bhawre <nandinibhawre24@gmailcom>
2026-05-29 15:48:40 +0530

    add security config

--- src/main/java/com/spring/chatApp/Security/SecurityConfig.java
+++ src/main/java/com/spring/chatApp/Security/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/ws/**",
-                                "/api/messages/**"
+                                "/api/messages/**",
+                                "api/friends/**"
                         ).permitAll()
 
                         .anyRequest().authenticated()
